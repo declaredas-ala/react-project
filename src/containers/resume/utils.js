ec734d3df9c20545e378c7c9a0b9933740ef5e1c
declaredas-ala <alamissaoui.dev@gmail.com>
2023-06-09 11:27:38 +0100

    Initial commit

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,27 @@
+export const data = {
+    experience: [
+      {
+        title: "",
+        subTitle: "",
+        description:
+          "",
+      },
+      
+    ],
+    education: [
+      {
+        title: "hight school",
+        subTitle: "",
+        description:
+          "baccaleaureat",
+      },
+      
+      
+      {
+        title: "software engineering degree",
+        subTitle: "faculty of science monastir",
+        description:
+          "Bachelor of technology in computer science",
+      },
+    ],
+  };
---
+++ src/containers/skills/utils.js
@@ -0,0 +1,54 @@
+export const skillsData = [
+    {
+      label: "FRONT END",
+      data: [
+        {
+          skillName: "HTML",
+          percentage: "90",
+        },
+        {
+          skillName: "CSS",
+          percentage: "50",
+        },
+        {
+          skillName: "bootsrap",
+          percentage: "70",
+        },
+        {
+          skillName: "React",
+          percentage: "40",
+        },
+        {
+          skillName: "JAVASCRIPT",
+          percentage: "80",
+        },
+      ],
+    },
+    {
+      label: "BACK END",
+      data: [
+        {
+          skillName: "Node.js ",
+          percentage: "30",
+        },
+       
+      ],
+    },
+    {
+      label: "DATABASES",
+      data: [
+        {
+          skillName: "SQL",
+          percentage: "70",
+        },
+        {
+          skillName: "MONGO DB",
+          percentage: "30",
+        },
+        {
+          skillName: "PostgreSQL",
+          percentage: "85",
+        },
+      ],
+    },
+  ];
